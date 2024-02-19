58cd4c6e1228b069cfe3b65371a3a6006a603783
Gustavo Bugs <gustavokbugs@gmail.com>
2024-02-19 17:22:23 -0300

    reset password screen (sketch)

--- src/app/user/reset-password/page.tsx
+++ src/app/user/reset-password/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "../../../components/ui/input";
 
-function LoginPage() {
+function RegisterPage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl text-center text-orange-500 mb-4">
@@ -13,43 +13,27 @@ function LoginPage() {
       <div className="bg-zinc-900 p-8 rounded-xl shadow-md">
         <p className="text-white text-2xl mb-4 text-center">
           <span className="border-b-2 border-orange-500 inline-block">
-            Welcome back!
+            Forgot your password?
           </span>
-        </p>
-        <p className="text-white text-sm mb-4">
-          Don't have an account yet?{" "}
-          <Link href="/user/register">
-            <span className="text-orange-500 underline cursor-pointer">
-              Click here
-            </span>
-          </Link>
-          .
         </p>
         <Input
           type="text"
           className="w-full mb-4 rounded-xl"
           placeholder="Email"
         />
-        <Input
-          type="password"
-          className="w-full mb-4 rounded-xl"
-          placeholder="Password"
-        />
-        <p className="text-white text-sm mb-4">
-          Forgot your password?{" "}
-          <Link href="/user/reset-password">
-            <span className="text-orange-500 underline cursor-pointer">
-              Click here
-            </span>
-          </Link>
-          .
+
+        <p className="text-xs text-zinc-500">
+          * We will send you a message to your email<br/>with the instructions to reset
+          your password
         </p>
+        <br/>
+
         <Button variant="outline" className="w-full rounded-3xl">
-          Login
+          Send
         </Button>
       </div>
     </div>
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
