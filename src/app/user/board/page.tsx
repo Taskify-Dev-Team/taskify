6501bde3e5598fcda66a12e196cc5063cd6fd6fd
Gustavo Bugs <gustavokbugs@gmail.com>
2024-02-27 19:04:37 -0300

    styling adjusts, colors settings and features added

--- src/app/user/board/page.tsx
+++ src/app/user/board/page.tsx
@@ -19,13 +19,15 @@ function TasksPage() {
       {/* Main content */}
       <div className="flex-1 flex flex-col">
         {/* Header */}
-        <header className="bg-zinc-800 text-white p-4 border-b border-orange-500">
+        <header className="bg-zinc-800 text-white p-4 border-b border-orange-500 relative">
           <div className="relative">
             <Input
               type="text"
               className="w-56 rounded-xl pl-8"
               placeholder="  Search tasks..."
             />
+            {/* Orange line */}
+            <div className="absolute inset-y-1 left-60 h-8 bg-orange-500 w-0.5"></div>
             <div className="absolute inset-y-0 left-2 flex items-center">
               <Image
                 className="ml-1"
@@ -39,7 +41,27 @@ function TasksPage() {
         </header>
 
         {/* Main content area */}
-        <main className="flex-1 p-4">{/* Your main content here */}</main>
+        <main className="flex p-4 gap-3">
+          <div className="flex-col">
+            <Input
+              type="text"
+              className="w-56 rounded-xl pl-8"
+              placeholder="Edit column name:"
+            />
+            <div className="absolute ml-2 mt-3 h-0.5 w-52 bg-orange-500"></div>
+
+            <Button
+              variant="default"
+              className="w-56 rounded-xl mt-6 bg-zinc-700"
+            >
+              +
+            </Button>
+          </div>
+
+          <Button variant="default" className="w-10 rounded-full text-zinc-900">
+            +
+          </Button>
+        </main>
 
         {/* Button with SVG */}
         <button className="absolute top-0 right-0 mr-2 mt-2">
